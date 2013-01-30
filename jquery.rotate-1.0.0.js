04ee65b0ebc77e7034719982f2e9c02d39cd30c4
Thomas Krammer <thomas.krammer@dotworx.at>
2013-01-31 00:06:51 +0100

    Changed version number to 1.0.0 to avoid conflicts. added minified version

--- jquery.rotate-1.0.0.js
+++ jquery.rotate-1.0.0.js
@@ -2,7 +2,7 @@
  * 
  * @author Thomas Krammer - thomas.krammer@dotworx.at
  * @copyright 2013 DOTWORX - Web- & Mobile Development KG
- * @version 0.1.1
+ * @version 1.0.0
  *
  * This plugin allows you to rotate n DOM element (e.g. div) by the amount of degrees given.
  *
